f4681a878941e26e98a8588e10ac750fc5404db5
bota_ <bota_@DESKTOP-E1RKMN8>
2021-04-02 13:35:28 -0700

    Flappy Bird

---
+++ Flappy/Flappy/src/flap/Renderer.java
@@ -0,0 +1,17 @@
+package flap;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel {
+
+private static final long serialVersionUID = 1L;
+
+    @Override
+	protected void paintComponent(Graphics g)
+	{
+		FlappyBird.flappyBird.repaint(g);
+	}
+	
+}
---
+++ Flappy/Flappy/src/module-info.java
@@ -0,0 +1,3 @@
+module Flappy {
+	requires java.desktop;
+}
